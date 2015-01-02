99ed1713d7a50180db6a239647e4230b90cf40c5
Abhishek Bhadange <abhishekbhadange@gmail.com>
2015-01-01 23:10:34 -0800

    Power function without * & /

---
+++ mathematicalAlgorithms/src/mathematicalAlgorithms/PowerFunction.java
@@ -0,0 +1,35 @@
+package mathematicalAlgorithms;
+
+public class PowerFunction {
+	public static void main(String[] args) {
+		int number = 8;
+		int power = 3;
+		System.out.println("Result: " + powerFunction(number, power));
+		System.out.println("Recursive result: " + recPower(number, power));
+	}
+	public static int powerFunction(int num, int pow) {
+		if(pow == 0)
+			return 1;
+		int answer = num;
+		int increment = num;
+		for(int i = 1; i < pow; i++) {
+			for(int j = 1; j < num; j++) {
+				answer += increment;
+			}
+			increment = answer;
+		}
+		return answer;
+	}
+	public static int recPower(int num, int pow) {
+		if(pow == 0)
+			return 1;
+		else
+			return multiply(num, recPower(num, pow - 1));
+	}
+	public static int multiply(int x, int y) {
+		if(y == 1)
+			return x;
+		else
+			return x + multiply(x, y - 1);
+	}
+}
