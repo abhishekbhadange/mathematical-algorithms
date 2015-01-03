771d5b7635962d6e7ab0a8f44130fc60f900acb3
Abhishek Bhadange <abhishekbhadange@gmail.com>
2015-01-03 11:49:53 -0800

    Exponentiation by squaring

--- mathematicalAlgorithms/src/mathematicalAlgorithms/EffiPowerFunction.java
+++ mathematicalAlgorithms/src/mathematicalAlgorithms/EffiPowerFunction.java
@@ -2,12 +2,15 @@ package mathematicalAlgorithms;
 
 public class EffiPowerFunction {
 	public static void main(String[] args) {
-		int base = 9, exp = 3;
+		int base = 9, exp = 4;
 		System.out.println("Efficient Power computation result: " + effiPowerFunction(base, exp));
+		System.out.println("Exponentiation by squaring result: " + expBySquaring(base, exp));
 	}
 	public static long effiPowerFunction(int base, int exp) {
 		if(exp == 0)
 			return 1;
+		if(exp == 1)
+			return base;
 		long result = 1;
 		while(exp != 0) {
 			if((exp & 1) != 0)
@@ -16,5 +19,15 @@ public class EffiPowerFunction {
 			base *= base;
 		}
 		return result;
+	}
+	public static int expBySquaring(int base, int exp) {
+		if(exp == 0)
+			return 1;
+		else if(exp == 1)
+			return base;
+		else if(exp % 2 == 0)
+			return expBySquaring(base*base, exp/2);
+		else
+			return base * expBySquaring(base*base, (exp - 1)/2);
 	}
 }
